96d7f8a4ba22cadb4b3286f93f9b48700ba2afb5
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-11-05 15:47:28 +0700

    jquery events

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-function func1() {
-  $("#img1").fadeToggle();
-}
+$(document).ready(function () {
+  let isLarge = false;
+  $("#btn").click(function () {
+    if (isLarge) {
+      $("#img1").css("width", "250px");
+    } else {
+      $("#img1").css("width", "500px");
+    }
+    isLarge = !isLarge;
+  });
+  // function func1() {
+  //   $("#img1").css("width", "500px");
+  // }
+  // $("#img1").on("dblclick", func1);
+  /*
+  $("#img1").mouseenter(function () {
+    $("#img1").css("width", "500px");
+  });
+  $("#img1").mouseleave(function () {
+    $("#img1").css("width", "250px");
+  });*/
+
+  $("#img1").hover(
+    function () {
+      $("#img1").css("width", "500px");
+    },
+    function () {
+      $("#img1").css("width", "250px");
+    }
+  );
+});
